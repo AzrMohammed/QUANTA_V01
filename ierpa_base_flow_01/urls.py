a2fd7793f9c04a28c7d0b70747b4da1212d1a055
AzrMohammed <azharmohammed090@gmail.com>
2021-09-17 18:28:56 +0530

    gffggg

--- ierpa_base_flow_01/urls.py
+++ ierpa_base_flow_01/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 
 # urlpatterns += staticfiles_urlpatterns()
 # + static(document_root=settings.STATIC_ROOT)
-              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
